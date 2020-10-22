54059c748cff24855ac4792c5e5c309d053fbca3
gaurav <gauraevg1234@gmail.com>
2020-10-23 00:20:45 +0530

    created seeder.js for import data to db

--- server/models/orderModel.js
+++ server/models/orderModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const productSchema = mongoose.Schema({
+const orderSchema = mongoose.Schema({
     user:{
         type: mongoose.Schema.Types.ObjectId,
         required: true,
---
+++ server/seeder.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const colors = require('colors')
+const users = require('./data/users')
+const products = require('./data/products')
+const User = require('./models/userModel')
+const Product = require('./models/productModel')
+const Order = require('./models/orderModel')
+const connectDB = require('./config/db')
+
+dotenv.config();
+
+connectDB();
+
+const importData = async() => {
+    try{
+        await User.deleteMany()
+        await Order.deleteMany()
+        await Product.deleteMany()
+
+        const createdUsers = await User.insertMany(users)
+        const adminUser = createdUsers[0]._id
+
+        const sampleProducts = products.map(product => {
+            return {
+                ...product, user:adminUser
+            }
+        })
+
+        await Product.insertMany(sampleProducts)
+        console.log('Data Imported')
+        process.exit()
+    }
+    catch(error){
+        console.error(error);
+        process.exit(1)
+    }
+}
+
+const destroyData = async() => {
+    try{
+        await User.deleteMany()
+        await Order.deleteMany()
+        await Product.deleteMany()
+
+        console.log('Data Destroyed')
+        process.exit()
+    }
+    catch(error){
+        console.error(error);
+        process.exit(1)
+    }
+}
+
+if(process.argv[2] === '-d'){
+    destroyData()
+}else{
+    importData()
+}
